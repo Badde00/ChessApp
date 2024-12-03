608b5ea6c72d98c6edf787d785696efc46095af4
Iris Flystam <badde00@gmail.com>
2024-12-03 15:26:51 +0100

    initial commit to chess app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChessApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to ChessApp!");
+      // Placeholder for future chess application logic
+    }
+  }
+}
